77a7f127dcb43c501f31ae8bf30e2cdb58b4a60f
Darshak Parikh <darshak@proton.me>
2025-05-05 00:20:42 +0530

    Use consistent name for WASD

--- states/settings.lua
+++ states/settings.lua
@@ -4,9 +4,12 @@ local settings = {}
 
 function settings:init()
     self.heading = love.graphics.newImage('assets/settings-heading.png')
-    self.keysArrows = love.graphics.newImage('assets/keys-arrows.png')
-    self.keysWSAD = love.graphics.newImage('assets/keys-wsad.png')
-    self.keysVim = love.graphics.newImage('assets/keys-vim.png')
+
+    self.keySettings = {
+        arrows = { x = 132, y = 188, image = love.graphics.newImage('assets/keys-arrows.png') },
+        wasd = { x = 332, y = 188, image = love.graphics.newImage('assets/keys-wasd.png') },
+        vim = { x = 532, y = 188, image = love.graphics.newImage('assets/keys-vim.png') },
+    }
     self.zoom1 = love.graphics.newImage('assets/zoom-1.png')
     self.zoom2 = love.graphics.newImage('assets/zoom-2.png')
     self.zoom3 = love.graphics.newImage('assets/zoom-3.png')
@@ -35,9 +38,9 @@ function settings:draw()
     love.graphics.draw(self.heading, headingX, headingY)
 
     -- Draw other settings elements here
-    love.graphics.draw(self.keysArrows, 132, 188)
-    love.graphics.draw(self.keysWSAD, 332, 188)
-    love.graphics.draw(self.keysVim, 532, 188)
+    love.graphics.draw(self.keySettings.arrows.image, self.keySettings.arrows.x, self.keySettings.arrows.y)
+    love.graphics.draw(self.keySettings.wasd.image, self.keySettings.wasd.x, self.keySettings.wasd.y)
+    love.graphics.draw(self.keySettings.vim.image, self.keySettings.vim.x, self.keySettings.vim.y)
     love.graphics.draw(self.zoom1, 172, 357)
     love.graphics.draw(self.zoom2, 350, 357)
     love.graphics.draw(self.zoom3, 550, 338)
